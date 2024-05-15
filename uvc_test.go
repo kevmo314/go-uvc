dd51602ca5854ceaa4172db1ed0330dc21ad994c
Juan <994594+juaoose@users.noreply.github.com>
2024-05-15 16:09:26 -0500

    replace uvc

--- uvc_test.go
+++ uvc_test.go
@@ -8,48 +8,6 @@ import (
 	"github.com/kevmo314/go-uvc/pkg/descriptors"
 )
 
-func TestDeviceInfoGoUSB(t *testing.T) {
-	fd, err := syscall.Open("/dev/bus/usb/001/002", syscall.O_RDWR, 0)
-	if err != nil {
-
-		t.Fatal(err)
-	}
-
-	uvcDevice, err := NewUVCDevice1(uintptr(fd))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	info, err := uvcDevice.GetDeviceInfo()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	for _, iface := range info.StreamingInterfaces {
-		for _, desc := range iface.Descriptors {
-			if _, ok := desc.(*descriptors.MJPEGFrameDescriptor); !ok {
-				continue
-			}
-
-			resp, err := iface.ClaimDeviceFrameReader(uvcDevice, info.bcdUVC, 0, 0)
-			if err != nil {
-				t.Fatal(err)
-			}
-			log.Printf("got negotiated format: %#v", resp)
-
-			// fr, err := resp.ReadFrame()
-			// if err != nil {
-			// 	t.Fatal(err)
-			// }
-			// log.Printf("got frame: %#v", fr)
-
-			break
-		}
-	}
-	t.Fail()
-
-}
-
 func TestDeviceInfo(t *testing.T) {
 	fd, err := syscall.Open("/dev/bus/usb/001/002", syscall.O_RDWR, 0)
 	if err != nil {
@@ -57,12 +15,12 @@ func TestDeviceInfo(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	ctx, err := NewUVCDevice(uintptr(fd))
+	device, err := NewUVCDevice(uintptr(fd))
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	info, err := ctx.DeviceInfo()
+	info, err := device.DeviceInfo()
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -74,17 +32,11 @@ func TestDeviceInfo(t *testing.T) {
 				continue
 			}
 
-			resp, err := iface.ClaimFrameReader(0, 0)
+			resp, err := iface.ClaimFrameReader(device, info.bcdUVC, 0, 0)
 			if err != nil {
 				t.Fatal(err)
 			}
 			log.Printf("got negotiated format: %#v", resp)
-
-			fr, err := resp.ReadFrame()
-			if err != nil {
-				t.Fatal(err)
-			}
-			log.Printf("got frame: %#v", fr)
 
 			break
 		}
